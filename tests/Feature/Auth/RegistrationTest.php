4c9f2d871d38789766e6a89c3879fd9c385f9b46
Isabella Firmino <isabella.mfbr@gmail.com>
2025-07-25 14:45:16 -0300

    update auth test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -16,6 +16,7 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'is_admin' => 'false'
         ]);
 
         $this->assertAuthenticated();
